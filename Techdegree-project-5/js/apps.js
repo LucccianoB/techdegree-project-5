e428bd2bc6e07b2411ea894f1b7c88fa46e8431a
LucccianoB <luccianobarberan@gmail.com>
2020-08-02 14:52:11 -0700

    search added

--- Techdegree-project-5/js/apps.js
+++ Techdegree-project-5/js/apps.js
@@ -1,1 +1,32 @@
-baguetteBox.run('.gallery');
+baguetteBox.run('.gallery', {
+    buttons: true,
+    animation: 'slideIn'
+});
+
+// searchbox
+
+//variables
+
+let searchinput = document.getElementById("imsearch").toLowerCase();
+const gallery = document.querySelector(".gallery");
+let ammount = gallery.getElementsByClassName("gallery-photos");
+let searchinput = '';
+let photocaption;
+
+function searchbox(searchinput) {
+
+    for (i= 0; i < ammount.length; i++ ) {
+
+        photocaption = ammount[i].dataset.caption.toLowerCase();
+
+        if(!photocaption.includes(searchinput)) {
+            ammount[i].style.display = "none";
+        } else {
+            photos[i].style.display = "";
+        }
+    }
+
+}
+
+
+
