4993fe0eefaf9d62197ba2adc1b2778534ebb7f0
LucccianoB <luccianobarberan@gmail.com>
2020-08-02 16:24:45 -0700

    added comments to serach function

--- Techdegree-project-5/js/apps.js
+++ Techdegree-project-5/js/apps.js
@@ -7,19 +7,21 @@ baguetteBox.run('.gallery', {
 
 //variables
 
-const searchbox = document.querySelector(".searchbox");
-let searchinput = '';
-const gallery = document.querySelector(".gallery");
-let ammount = gallery.getElementsByClassName("gallery-photos");
+const searchbox = document.querySelector(".searchbox"); /* for eventlistener to input live search*/
+let searchinput = ''; /* what is typed into the searchbar*/
+const gallery = document.querySelector(".gallery"); 
+let ammount = gallery.getElementsByClassName("gallery-photos"); /*used in order to serach through and speicify the photos*/
 let photocaption;
+
+//function
 
 function search(searchinput) {
 
-    for (i= 0; i < ammount.length; i++ ) {
+    for (i= 0; i < ammount.length; i++ ) {  /* searches through all the photos*/
 
-        photocaption = ammount[i].dataset.caption.toLowerCase();
+        photocaption = ammount[i].dataset.caption.toLowerCase(); /*checks the caption of the given photo*/
 
-        if(!photocaption.includes(searchinput)) {
+        if(!photocaption.includes(searchinput)) {  /* checks to see if it includes what is inputed into the searchbar */
             ammount[i].style.display = "none";
         } else {
             ammount[i].style.display = "";
